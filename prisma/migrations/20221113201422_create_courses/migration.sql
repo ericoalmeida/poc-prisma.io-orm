ca1bf04f0bb65e26ed2611313868996119ea3a53
Erico Almeida <erico.silva@invillia.com>
2022-11-13 16:16:03 -0400

    feat: add courses database table

---
+++ prisma/migrations/20221113201422_create_courses/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "courses" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "duration" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "courses_name_key" ON "courses"("name");
