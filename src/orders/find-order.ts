1038856c60db7ff1b507ddbbaa484afdd48715ab
Erico Almeida <erico.silva@invillia.com>
2022-11-23 07:08:56 -0400

    refactor: Change fields returned on find order method

--- src/orders/find-order.ts
+++ src/orders/find-order.ts
@@ -3,5 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 export const findOrder = async (id: string) => {
-  return client.orders.findFirst({ where: { id } });
+  return client.orders.findFirst({
+    where: { id },
+    select: {
+      id: true,
+      date: true,
+      customer_id: true,
+      total: true,
+      items: {
+        select: {
+          product_id: true,
+          amount: true,
+          total: true,
+        },
+      },
+    },
+  });
 };
